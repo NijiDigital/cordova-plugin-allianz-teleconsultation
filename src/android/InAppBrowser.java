9b23090505427ab7f3a395922b9b73db8420cb8b
Bertrand PRESLES <bertrand@presles.fr>
2019-05-22 11:54:10 +0200

    Change Java package name

--- src/android/InAppBrowser.java
+++ src/android/InAppBrowser.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.inappbrowser;
+package fr.niji.cordova.allianzteleconsultation;
 
 import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
--- src/android/InAppChromeClient.java
+++ src/android/InAppChromeClient.java
@@ -16,7 +16,7 @@
        specific language governing permissions and limitations
        under the License.
 */
-package org.apache.cordova.inappbrowser;
+package fr.niji.cordova.allianzteleconsultation;
 
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.LOG;
